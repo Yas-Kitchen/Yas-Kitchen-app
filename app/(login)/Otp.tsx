d932b688c4c4d91a75a84c9d8d63bf5ec9187fc4
Adithya kiran C B <adithyakiran021@gmail.com>
2025-09-22 15:25:55 +0530

    login page finished

---
+++ app/(login)/Otp.tsx
@@ -0,0 +1,12 @@
+import { Feather } from '@expo/vector-icons'
+import React from 'react'
+
+const Otp = () => {
+  return (
+    <View>
+    <Feather name='chevron-left'/>
+    </View>
+  )
+}
+
+export default Otp
--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Image, View } from 'react-native'
+import { router } from "expo-router";
+import React from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 const index = () => {
   return (
-<View className='h-screen m-auto items-center justify-center'>
-    <Image className='w-24 h-24 ' source={ require("../../assets/Login/unfixed_logo.jpeg")}/>
-</View>
-  )
-}
+    <View className="h-screen justify-center">
+      <Image
+        className="w-36 h-36 mx-auto"
+        source={require("../../assets/Login/unfixed_logo.png")}
+      />
+      <Text className="text-[26px] font-bold mx-auto">Yas Kitchen</Text>
+      <Text className="text-[13px] text-base mx-auto">
+        Delicious meals delivered daily
+      </Text>
+      <View className="mt-20 mx-10 border border-[#bababa27] rounded-2xl p-5 ">
+        <Text className="text-[17px] font-semibold mx-auto">Login Via Phone</Text>
+        <View className="mt-10 mx-5">
+          <Text className="text-base font-medium text-[12px]">
+            Mobile Number
+          </Text>
+          <TextInput
+            className="p-5 bg-white rounded-2xl mt-2"
+            placeholder="+91 9182736450"
+          />
+          <TouchableOpacity onPress={() => router.push("/(login)/Otp")} className="bg-primary mt-5 p-5 rounded-2xl">
+            <Text className="text-center text-white">Continue</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+          <TouchableOpacity className=" mt-5 flex-row justify-center">
+            <Text className="text-center text-black">New to yas kitchen ? </Text>
+            <Text className="text-primary"> Register Now</Text>
+          </TouchableOpacity>
+    </View>
+  );
+};
 
-export default index
+export default index;
